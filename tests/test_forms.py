f3508aa348aa6f560953cbf48c6671ccf8558410
funkybob <curtis@tinbrain.net>
2014-11-12 12:11:24 +1100

    Make first field test work

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,14 +1,23 @@
 
+from django import forms
 from django.template.loader import get_template
 from django.test import SimpleTestCase
 
 from .utils import TemplateTestMixin
 
 
+class TestForm(forms.Form):
+    char = forms.CharField()
+
+
 class TestFieldTag(TemplateTestMixin, SimpleTestCase):
     TEMPLATES = {
-        'field': '{% load sniplates %}{% form_field form.field %}'
+        'widgets': '''{% block CharField %}<input type="text" name="{{ name }}" value="{{ value|default:'' }}>{% endblock %}''',
+        'field': '{% load sniplates %}{% load_widgets form="widgets" %}{% form_field form.char %}'
     }
+    def setUp(self):
+        super(TestFieldTag, self).setUp()
+        self.ctx['form'] = TestForm()
 
     def test_field_tag(self):
         '''
