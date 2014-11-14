9af2c0becc3076575dba96418905f86467fd8265
funkybob <curtis@tinbrain.net>
2014-11-14 14:15:13 +1100

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-sniplates',
-    version='0.1.1',
+    version='0.2.0',
     description='Efficient template macro sets for Django',
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
