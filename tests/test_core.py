2636edaadc74edd50ecbc75af305676926902157
Matthew Schinckel <matt@schinckel.net>
2015-05-23 21:34:05 +0930

    Cleanup inheritance test locations.

--- tests/test_core.py
+++ tests/test_core.py
@@ -78,39 +78,3 @@ class TestWidgetTag(TemplateTestMixin, SimpleTestCase):
         output = tmpl.render(self.ctx)
 
         self.assertEqual(output, 'more value\n')
-
-
-@override_settings(
-    TEMPLATE_DIRS=[template_path('inheritance')],
-)
-class TestInheritance(TemplateTestMixin, SimpleTestCase):
-
-    def test_block_overlap(self):
-        '''
-        Ensure that when we reference a block from a sniplate that doesn't
-        exist, but is in our template, it isn't used.
-        '''
-        tmpl = get_template('block_overlap')
-
-        with self.assertRaises(TemplateSyntaxError):
-            tmpl.render(self.ctx)
-
-    def test_parent_inherit(self):
-        '''
-        When our parent template loads sniplates, we should have access to them
-        also.
-        '''
-        tmpl = get_template('parent_inherit')
-        output = tmpl.render(self.ctx)
-
-        self.assertEqual(output, 'foo\n')
-
-    def test_parent_overlap(self):
-        '''
-        If a sniplate library has a block of the same name as in the calling
-        template, we should NOT override it.
-        '''
-        tmpl = get_template('parent_overlap')
-        output = tmpl.render(self.ctx)
-
-        self.assertEqual(output, 'first\n')
--- tests/test_inherited.py
+++ tests/test_inherited.py
@@ -1,4 +1,4 @@
-
+from django.template import TemplateSyntaxError
 from django.template.loader import get_template
 from django.test import SimpleTestCase
 
@@ -8,7 +8,37 @@ from .utils import TemplateTestMixin, template_path, override_settings
 @override_settings(
     TEMPLATE_DIRS=[template_path('inheritance')],
 )
-class TestInheritanceTag(TemplateTestMixin, SimpleTestCase):
+class TestInheritance(TemplateTestMixin, SimpleTestCase):
+
+    def test_block_overlap(self):
+        '''
+        Ensure that when we reference a block from a sniplate that doesn't
+        exist, but is in our template, it isn't used.
+        '''
+        tmpl = get_template('block_overlap')
+
+        with self.assertRaises(TemplateSyntaxError):
+            tmpl.render(self.ctx)
+
+    def test_parent_inherit(self):
+        '''
+        When our parent template loads sniplates, we should have access to them
+        also.
+        '''
+        tmpl = get_template('parent_inherit')
+        output = tmpl.render(self.ctx)
+
+        self.assertEqual(output, 'foo\n')
+
+    def test_parent_overlap(self):
+        '''
+        If a sniplate library has a block of the same name as in the calling
+        template, we should NOT override it.
+        '''
+        tmpl = get_template('parent_overlap')
+        output = tmpl.render(self.ctx)
+
+        self.assertEqual(output, 'first\n')
 
     def test_block_super(self):
         tmpl = get_template('super')
