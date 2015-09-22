10125239b2c411a7ec03db67e895eef0fb9de094
Sergei Maertens <sergei@maykinmedia.nl>
2015-09-22 10:09:33 +0200

    Add test describing regression
    For fields with multiple values, the `value` cached property
    must take non-scalar raw_values into account. Effectively,
    `raw_value = (11, 22)` should become `value = ('11', '22')` and
    not `value = '(11, 22)'`. When a choice exists with the value
    a substring (after force_text) of a selected choice, this first
    choice will incorrectly be marked as selected because the string
    is contained the string `value`.

--- tests/forms.py
+++ tests/forms.py
@@ -6,3 +6,4 @@ class TestForm(forms.Form):
     char = forms.CharField()
     oneof = forms.ChoiceField(choices=tuple(enumerate('abcd')))
     many = forms.MultipleChoiceField(choices=tuple(enumerate('abcd')))
+    many2 = forms.MultipleChoiceField(choices=((1, 'a'), (11, 'b'), (22, 'c')))
