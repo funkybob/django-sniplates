c381ef7643513e839faeaf4f8f2a151d971642ea
Sergei Maertens <sergei@maykinmedia.nl>
2016-01-25 18:48:32 +0100

    Add test for bound form - few failing widgets

--- tests/test_widgets_django.py
+++ tests/test_widgets_django.py
@@ -1,9 +1,11 @@
 """
 Tests for all shipped sniplates/django.html widgets.
 """
+import datetime
 
 from django.template.loader import get_template
 from django.test import SimpleTestCase, override_settings
+from django.utils.datastructures import MultiValueDict
 
 from .forms import DjangoWidgetsForm
 from .utils import TemplateTestMixin, template_path
@@ -12,15 +14,12 @@ from .utils import TemplateTestMixin, template_path
 @override_settings(TEMPLATE_DIRS=[template_path('field_tag')])
 class TestFieldTag(TemplateTestMixin, SimpleTestCase):
 
-    def setUp(self):
-        super(TestFieldTag, self).setUp()
-        self.ctx['form'] = DjangoWidgetsForm()
-
     def test_widgets_unbound(self):
         """
         Tests all form fields one by one, with a fresh, unbound form without
         initial data.
         """
+        self.ctx['form'] = DjangoWidgetsForm()
         tmpl = get_template('widgets_django')
         output = tmpl.render(self.ctx)
 
@@ -33,11 +32,12 @@ class TestFieldTag(TemplateTestMixin, SimpleTestCase):
             'password': '<input type="password" name="password" id="id_password" value="" class=" " required>',
             'hidden': '<input type="hidden" name="hidden" id="id_hidden" value="" class=" " required>',
             # this one is hard to test, as it may NOT contain the output - it's empty by default
-            'multiple_hidden': '''
-                <input type="hidden" name="multiple_hidden" id="id_multiple_hidden_0" value="N" required>
-                <input type="hidden" name="multiple_hidden" id="id_multiple_hidden_1" value="o" required>
-                <input type="hidden" name="multiple_hidden" id="id_multiple_hidden_2" value="n" required>
-                <input type="hidden" name="multiple_hidden" id="id_multiple_hidden_3" value="e" required>''',
+            'multiple_hidden': [
+                '<input type="hidden" name="multiple_hidden" id="id_multiple_hidden_0" value="N" required>',
+                '<input type="hidden" name="multiple_hidden" id="id_multiple_hidden_1" value="o" required>',
+                '<input type="hidden" name="multiple_hidden" id="id_multiple_hidden_2" value="n" required>',
+                '<input type="hidden" name="multiple_hidden" id="id_multiple_hidden_3" value="e" required>',
+            ],
             'date': '<input type="date" name="date" id="id_date" value="" class=" " required>',
             'datetime': '<input type="datetime" name="datetime" id="id_datetime" value="" class=" " required>',
             'time': '<input type="time" name="time" id="id_time" value="" class=" " required>',
@@ -92,8 +92,8 @@ class TestFieldTag(TemplateTestMixin, SimpleTestCase):
         self.assertInHTML(expected_output['password'], output, msg_prefix='PasswordInput rendered incorrectly: ')
         self.assertInHTML(expected_output['hidden'], output, msg_prefix='HiddenInput rendered incorrectly: ')
 
-        self.assertNotInHTML(
-            expected_output['multiple_hidden'], output, msg_prefix='MultipleHiddenInput rendered incorrectly: ')
+        for input_ in expected_output['multiple_hidden']:
+            self.assertNotInHTML(input_, output, msg_prefix='MultipleHiddenInput rendered incorrectly: %s: ' % input_)
         self.assertInHTML(expected_output['date'], output, msg_prefix='DateInput rendered incorrectly: ')
         self.assertInHTML(expected_output['datetime'], output, msg_prefix='DateTimeInput rendered incorrectly: ')
         self.assertInHTML(expected_output['time'], output, msg_prefix='TimeInput rendered incorrectly: ')
@@ -112,3 +112,112 @@ class TestFieldTag(TemplateTestMixin, SimpleTestCase):
             msg_prefix='CheckboxSelectMultiple rendered incorrectly: '
         )
         self.assertInHTML(expected_output['file'], output, msg_prefix='FileInput rendered incorrectly: ')
+
+    def test_widgets_bound(self):
+        """
+        Tests all form fields one by one, with a bound form.
+        """
+        self.ctx['form'] = DjangoWidgetsForm(data=MultiValueDict({
+            'char': ['test char'],
+            'email': ['foo@bar.com'],
+            'url': ['https://example.com'],
+            'number': [42],
+            'password': ['secret'],
+            'hidden': ['peek-a-boo'],
+            'multiple_hidden': ['first', 'second'],
+            'date': [datetime.date(2016, 1, 25)],
+            'datetime': [datetime.datetime(2016, 1, 25, 9, 0, 42)],
+            'time': [datetime.time(8, 9)],
+            'text': ['Lorem ipsum...'],
+            'checkbox': [True],
+            'select': '22',
+            'null_boolean_select': [False],
+            'select_multiple': ['11', '22', 1],
+            'radio_select': ['11'],
+            'checkbox_select_multiple': ['1', 11],
+            # TODO: file/clearable_file
+        }))
+        tmpl = get_template('widgets_django')
+        output = tmpl.render(self.ctx)
+
+        expected = {
+            'char': '<input type="text" name="char" id="id_char" value="test char" class=" " required>',
+            'email': '<input type="email" name="email" id="id_email" value="foo@bar.com" class=" " required>',
+            'url': '<input type="url" name="url" id="id_url" value="https://example.com" class=" " required>',
+            'number': '<input type="number" name="number" id="id_number" value="42" class=" " required>',
+            'password': '<input type="password" name="password" id="id_password" value="secret" class=" " required>',
+            'hidden': '<input type="hidden" name="hidden" id="id_hidden" value="peek-a-boo" class=" " required>',
+            'multiple_hidden': [
+                '<input type="hidden" name="multiple_hidden" id="id_multiple_hidden_0" value="first" required>',
+                '<input type="hidden" name="multiple_hidden" id="id_multiple_hidden_1" value="second" required>',
+            ],
+            'date': '<input type="date" name="date" id="id_date" value="2016-01-25" class=" " required>',
+            'datetime': '<input type="datetime" name="datetime" id="id_datetime" value="2016-01-25 09:00:42" class=" " required>',
+            'time': '<input type="time" name="time" id="id_time" value="08:09:00" class=" " required>',
+            'text': '<textarea name="text" id="id_text" class=" " required cols="40" rows="10">Lorem ipsum...</textarea> ',
+            'checkbox': '''
+                <label for="id_checkbox" class="">
+                    <input name="checkbox" id="id_checkbox" type="checkbox" checked>
+                    Checkbox
+                </label>''',
+            'select': '''
+                <select name="select" id="id_select">
+                <option value="1">a</option>
+                <option value="11">b</option>
+                <option value="22" selected>c</option>
+                </select>''',
+            'null_boolean_select': '''
+                <select id="id_null_boolean_select" name="null_boolean_select">
+                <option value="1" selected="selected">Unknown</option>
+                <option value="2">Yes</option>
+                <option value="3" selected>No</option>
+                </select>''',
+            'select_multiple': '''
+                <select name="select_multiple" id="id_select_multiple" multiple>
+                    <option value="1" selected>a</option>
+                    <option value="11" selected>b</option>
+                    <option value="22" selected>c</option>
+                </select>''',
+            'radio_select': '''
+                <ul id="id_radio_select">
+                    <li><input name="radio_select" type="radio" id="id_radio_select_0" value="1" >a</li>
+                    <li><input name="radio_select" type="radio" id="id_radio_select_1" value="11" checked>b</li>
+                    <li><input name="radio_select" type="radio" id="id_radio_select_2" value="22" >c</li>
+                </ul>''',
+            'checkbox_select_multiple': '''
+                <ul id="id_checkbox_select_multiple">
+                    <li><input name="checkbox_select_multiple" type="checkbox"
+                        id="id_checkbox_select_multiple_0" value="1" checked>a</li>
+                    <li><input name="checkbox_select_multiple" type="checkbox"
+                        id="id_checkbox_select_multiple_1" value="11" checked>b</li>
+                    <li><input name="checkbox_select_multiple" type="checkbox"
+                        id="id_checkbox_select_multiple_2" value="22">c</li>
+                </ul>''',
+            'file': '<input id="id_file" name="file" type="file" value="" class=" " required>',
+            'clearable_file': '''
+                <input id="id_clearable_file" name="clearable_file" type="file" value="" class=" " required>''',
+        }
+
+        self.assertInHTML(expected['char'], output)
+        self.assertInHTML(expected['email'], output)
+        self.assertInHTML(expected['url'], output)
+        self.assertInHTML(expected['number'], output)
+        self.assertInHTML(expected['password'], output)
+        self.assertInHTML(expected['hidden'], output)
+        for input_ in expected['multiple_hidden']:
+            self.assertInHTML(input_, output)
+        self.assertInHTML(expected['text'], output)
+        self.assertInHTML(expected['checkbox'], output)
+
+        self.assertInHTML(expected['select_multiple'], output)
+        self.assertInHTML(expected['radio_select'], output)
+        self.assertInHTML(expected['checkbox_select_multiple'], output)
+        # self.assertInHTML(expected['file'], output)
+        # self.assertInHTML(expected['clearable_file'], output)
+
+        # failing checks: FIXME
+        self.assertInHTML(expected['date'], output)
+        self.assertInHTML(expected['datetime'], output)
+        self.assertInHTML(expected['time'], output)
+        self.assertInHTML(expected['select'], output)
+        self.assertInHTML(expected['null_boolean_select'], output)
