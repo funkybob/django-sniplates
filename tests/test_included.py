7a8b535cf231d579f9cc970459608154c1280842
Matthew Schinckel <matt@schinckel.net>
2017-05-22 16:55:46 +0930

    Simpler way of testing!
    Didn't need to patch the function and count: can infer from the failure to render
    that the widgets are not loaded.

--- tests/test_included.py
+++ tests/test_included.py
@@ -2,11 +2,6 @@
 from django.template.loader import get_template
 from django.test import SimpleTestCase
 
-try:
-    from unittest.mock import patch
-except ImportError:
-    from mock import patch
-
 from .utils import TemplateTestMixin, template_dirs
 
 
@@ -17,9 +12,3 @@ class TestIncluded(TemplateTestMixin, SimpleTestCase):
         tmpl = get_template('base')
         output = tmpl.render(self.ctx)
         self.assertEqual(output.strip(), 'FOO')
-
-    @patch('sniplates.templatetags.sniplates.resolve_blocks')
-    def test_ensure_soft_only_executes_once_on_include(self, resolve_blocks):
-        tmpl = get_template('base')
-        tmpl.render(self.ctx)
-        self.assertEqual(resolve_blocks.call_count, 1)
