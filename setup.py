5399ba1bf901b9b84a576f8910df37e168d1086f
Curtis Maloney <curtis@tinbrain.net>
2020-01-22 08:10:12 +1100

    Bump version to 0.7.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-sniplates',
-    version='0.6.0',
+    version='0.7.0',
     description='Efficient template macro sets for Django',
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
