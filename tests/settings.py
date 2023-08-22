125874a180c60b8b9c4ffcac9a181887fafef43a
Curtis Maloney <curtis@tinbrain.net>
2023-08-22 16:45:33 +1000

    Move settings to tests/settings.py

---
+++ tests/settings.py
@@ -0,0 +1,24 @@
+DATABASES={
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+
+INSTALLED_APPS=(
+    'sniplates',
+    'tests',
+)
+
+MIDDLEWARE_CLASSES=[]
+
+TEMPLATES=[
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'debug': True
+        }
+    },
+]
+
